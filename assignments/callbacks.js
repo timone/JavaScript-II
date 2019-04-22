e54de4956008bd148b52bf397e65d2495e1f0b34
Timur Mone <timur@yurtah.com>
2019-04-21 20:45:21 -0400

    fixed bugs in callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -3,70 +3,72 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 /* 
-
   //Given this problem: 
   
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
-
   // Potential Solution:
-
   // Higher order function using "cb" as the call back
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
-
   // Function invocation 
   firstItem(items, function(first) {
     console.log(first)
   });
-
 */
-function  master(arr,cb){
-  console.log(cb(arr));
-}
-function  numbers(x,y,  cb){
-  console.log(cb(x,y));
-}
 
 
- 
-function  getLength(arr, cb){
+function getLength(arr, cb) 
+{
   // getLength passes the length of the array into the callback.
-  return  arr[0];
+  return cb(arr.length);
 }
-master(items,getLength);
+getLength(items, function(length) {
+  console.log(length)
+});
 
-function  last(arr, cb){
+function last(arr, cb) 
+{
   // last passes the last item of the array into the callback.
-return  arr[arr.length-1];
+  return cb(arr.pop());
 }
-master(items,last);
+last(items, function(lastitem) {
+  console.log(lastitem)
+});
 
-function  sumNums(x, y, cb){
+function sumNums(x, y, cb) 
+{
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return  x + y;
+  return cb(x+y);
 }
-numbers (1,2,sumNums);
+sumNums(3, 4, function(numbers) {
+  console.log(numbers)
+});
 
-function  multiplyNums(x, y, cb){
+function multiplyNums(x, y, cb) 
+{
   // multiplyNums multiplies two numbers and passes the result to the callback.
-return  x * y;
+  return cb(x*y);
 }
-numbers (3,4,multiplyNums);
+multiplyNums(3, 4, function(number){
+  console.log(number)
+});
 
-function  contains(item, list, cb){
+function contains(item, list, cb) 
+{
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  return  list.includes (item);
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === item) {return true}
+      else return false;
+  }
 }
-
-numbers ('Pencil', items, contains);
 
 /* STRETCH PROBLEM */
 
-function  removeDuplicates(array, cb){
+function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
